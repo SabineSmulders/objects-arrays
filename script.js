9664d842e2d41085224f331688d3afa078c780ab
SabineSmulders <92474494+SabineSmulders@users.noreply.github.com>
2022-01-06 20:50:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// Part 1 Objects
+
+const person = {
+    name: "Sabine",
+    age: 25
+};
+
+console.log(person);
+
+//Dot nonation 
+person.name = "Sabine"
+console.log(person.name);
+
+//Bracket notation 
+person['age'] = '25';
+console.log(person.age)
+
+const evalutions = {
+    marks: [7, 10, 9]
+}
+
+console.log(marks)
+
+// Part 2 Arrays
+
+const selectedColors = ['green', 'orange', 'pink', 'black', '5']
+selectedColors[2] = 5
+selectedColors[3] = (greeting, "hi I am an object")
+console.log('green')
+console.log([3])
+console.log(selectedColors.length);
+
+// Part 3 A "real-life" object
+catBreeds.name = 'American Brobtail'
+console.log(catBreeds.name)
+
+catBreeds.energy_level = '1.5'
+console.log(catBreeds.energy_level)
+
+catBreeds.temperament = 'Affectionate'
+console.log(catBreeds.temperament)
+
+catBreeds.temperament = 'Sensitive'
+console.log(catBreeds.temperament)
+
+catBreeds.food = 'meaty things'
+console.log(catBreeds.food)
